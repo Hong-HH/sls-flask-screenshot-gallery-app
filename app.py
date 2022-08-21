8a699954b8b9e5ddec403f09d9a599313ab1a2b9
Hong-HH <a1guskn7@gmail.com>
2022-08-21 19:31:33 +0900

    app.py_moved

--- app.py
+++ app.py
@@ -1,18 +1,60 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, request
+# JWT 사용을 위한 SECRET_KEY 정보가 들어있는 파일 임포트
+from config import Config
+from flask.json import jsonify
+from http import HTTPStatus
+
+from flask_restful import Api
+
+from flask_jwt_extended import JWTManager
+
+from resources.register import UserRegisterResource
+from resources.login import UserLoginResource
+from resources.logout import LogoutResource
+from resources.blocklist import check_blocklist
+from resources.photo import PhotoResource
+from resources.photo_change import PhotoChangeResource
+from resources.rekognition import TagResource
+
 
 app = Flask(__name__)
 
+# 환경 변수 세팅
+app.config.from_object(Config)
 
-@app.route("/")
-def hello_from_root():
-    return jsonify(message='Hello from root!')
+# JWT 토큰 만들기
+jwt = JWTManager(app)
+
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload) :
+    # db 에서 해당 jti 가 있는지 확인한다.
+    jti = jwt_payload['jti']
+    result = check_blocklist(jti)
+
+    return result
 
 
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
+api = Api(app)
+
+# resources 와 연결
+api.add_resource(UserRegisterResource, '/v1/user/register')     # 회원가입
+api.add_resource(UserLoginResource, '/v1/user/login')           # 로그인
+api.add_resource(LogoutResource, '/v1/user/logout')             # 로그아웃
+api.add_resource(PhotoResource, '/v1/photo')                    # 사진 업로드 / 조회
+api.add_resource(PhotoChangeResource, '/v1/photo/<int:photo_id>')# 사진의 설명 변경, 사진 삭제
+api.add_resource(TagResource, '/v1/tag/<int:photo_id>')         # 사진의 태그 및 설명
 
 
-@app.errorhandler(404)
-def resource_not_found(e):
-    return make_response(jsonify(error='Not found!'), 404)
+
+# 기본 루트 연결 확인 멘트
+@app.route('/')
+def route_page():
+    return "hello!!!!! it is work~"
+
+
+
+
+if __name__ == "__main__" :
+    app.run()
+
+
